fac0cf448da658f0afa4f9afa2f77d6091b29a1d
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2019-07-26 21:19:52 -0500

    Nodemon watch folder changed.

--- ormconfig.js
+++ ormconfig.js
@@ -8,6 +8,8 @@ module.exports = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  synchronize: true,
+  dropSchema: false,
   logging: true,
   entities: ['src/**/*.entity.ts', 'dist/**/*.entity.js'],
 };
